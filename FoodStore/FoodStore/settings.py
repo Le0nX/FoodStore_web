daa6fb3a0f99db4cf84a8a9a44305d7f4a4e0c44
Le0nX <cloudmindone@gmail.com>
2018-08-20 11:50:27 +0300

    added .gitignore and template for secret settings

--- FoodStore/FoodStore/settings.py
+++ FoodStore/FoodStore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@s!dl!3e4)&x-t03!2(^0!v2x=+h$@$r6h2f2^gq$kq87a!f7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
