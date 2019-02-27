1e139aaf4b7a293f482415bcfaf71365c3993c14
Le0nX <cloudmindone@gmail.com>
2019-02-27 16:17:06 +0300

    feature: added db support for signup

--- foodstore/foodstore/urls.py
+++ foodstore/foodstore/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from foodstoreapp import views
 from django.contrib.auth import views as auth_views
 
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
@@ -30,4 +34,4 @@ urlpatterns = [
     url(r'^restaurant/$', views.restaurant_home, name='restaurant-home'),
     url(r'^restaurant/sign-up/$', views.restaurant_sign_up,
         name='restaurant-sign-up'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
